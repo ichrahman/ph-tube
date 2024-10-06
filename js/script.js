d3bdd96e96f15fd16f51ccb30f7bfa4acb4dea69
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-10-06 20:37:20 +0200

    Add video fetching and dynamic card-based video display

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const loadCategories = () => {
         .then(data => displayCategories(data.categories))
 }
 
+
+
 // display categories
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById("categories");
@@ -21,4 +23,39 @@ const displayCategories = (categories) => {
     })
 }
 
+// load videos
+const loadVideos = () => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos`)
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+}
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos');
+
+    videos.forEach(video => {
+        console.log(video)
+        const card = document.createElement("div");
+        card.classList = "card card-compact"
+        card.innerHTML =
+            ` 
+        <figure>
+            <img
+            src=${video.thumbnail}
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${video.title}</h2>
+            <p>${video.description}</p>
+        </div>`;
+
+        videoContainer.append(card);
+    })
+}
+
+
+
+
+
 loadCategories()
+loadVideos()
