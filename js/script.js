9cd4806105ff4afccdc7a3cd9ff3a69f072082d8
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-10-07 00:06:53 +0200

    Add dynamic time and video information display with category and author details

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-// todo: fetch categories and display
+
+
+function getTimeString(time) {
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+
 
 // load categories
 const loadCategories = () => {
@@ -60,11 +69,12 @@ const displayVideos = (videos) => {
         card.classList = "card card-compact"
         card.innerHTML =
             ` 
-        <figure class="h-[200px]">
+        <figure class="h-[200px] relative">
             <img
             src=${video.thumbnail}
             class="h-full w-full object-cover"
             alt="Shoes" />
+            ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)}</span>`}
         </figure>
         <div class="px-0 py-2 flex gap-2">
             <div>
@@ -74,7 +84,7 @@ const displayVideos = (videos) => {
                 <h2 class="font-bold">${video.title}</h2>
                 <div class="flex items-center gap-2">
                     <p class="text-gray-400">${video.authors[0].profile_name}</p>
-                    <img class="w-5" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000"/>
+                    ${video.authors[0].verified ? `<img class="w-5" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" />` : ""}
                 </div>
                 <p></p>
             </div>
